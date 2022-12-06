f41d94caaed4165e268a3ab4c2d0cc1242ed6e83
Yevgen Berberyan <egnbjork@users.noreply.github.com>
2022-12-06 13:15:47 +0100

    day 6 part 2

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,13 +1,16 @@
+const val DISTINCT_CHARS = 14
+
 fun main() {
     val gameInput = readInput("Day06_test")
 
     val marker = mutableListOf<Char>()
     for((index, value) in gameInput[0].toCharArray().withIndex()) {
         marker.add(value)
-        if(marker.size == 4 && marker.distinct().size == 4) {
+        if(marker.size == DISTINCT_CHARS &&
+            marker.distinct().size == DISTINCT_CHARS) {
             println(index + 1)
             return
-        } else if(marker.size == 4){
+        } else if(marker.size == DISTINCT_CHARS){
             marker.removeFirst()
         }
     }
