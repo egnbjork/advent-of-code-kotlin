3900c03c573bf8139fecbd137b275a8cc085d672
Yevgen Berberyan <egnbjork@users.noreply.github.com>
2022-12-10 19:08:22 +0100

    day 9 part 2

--- src/Day09.kt
+++ src/Day09.kt
@@ -31,56 +31,65 @@ fun parseSteps(input: List<String>): List<Pair<Char, Int>> {
 
 class Rope {
     var head = Pair(0, 0)
-    var tail = Pair(0, 0)
+    var tail = MutableList(9) { 0 to 0 }
     var positionVisited = mutableSetOf <Pair<Int, Int>>()
 
     fun right(steps: Int) {
         repeat(steps) {
             head = head.copy(head.first, head.second + 1)
-            tail = getTailPosition()
-            positionVisited.add(tail)
+            loopTail()
+            positionVisited.add(tail.last())
         }
     }
 
     fun up(steps: Int) {
         repeat(steps) {
             head = head.copy(head.first + 1, head.second)
-            tail = getTailPosition()
-            positionVisited.add(tail)
+            loopTail()
+            positionVisited.add(tail.last())
         }
     }
 
     fun left(steps: Int) {
         repeat(steps) {
             head = head.copy(head.first, head.second - 1)
-            tail = getTailPosition()
-            positionVisited.add(tail)
+            loopTail()
+            positionVisited.add(tail.last())
         }
     }
 
     fun down(steps: Int) {
         repeat(steps) {
             head = head.copy(head.first - 1, head.second)
-            tail = getTailPosition()
-            positionVisited.add(tail)
+            loopTail()
+            positionVisited.add(tail.last())
         }
     }
 
-    private fun getTailPosition(): Pair<Int, Int> {
-        val xDistance = head.first - tail.first
-        val yDistance = head.second - tail.second
+    private fun loopTail() {
+        tail.forEachIndexed { i, v ->
+            if(i == 0) {
+                tail[0] = getTailPosition(head, tail[0])
+            } else {
+                tail[i] = getTailPosition(tail[i - 1], tail[i])
+            }
+        }
+    }
+    private fun getTailPosition(start: Pair<Int, Int>, end: Pair<Int, Int>): Pair<Int, Int> {
+        val xDistance = start.first - end.first
+        val yDistance = start.second - end.second
 
         if (xDistance == 0 && abs(yDistance) == 2) {
-            return Pair(tail.first, (tail.second + yDistance / 2))
+            return Pair(end.first, (end.second + yDistance / 2))
         }
         if (yDistance == 0 && abs(xDistance) == 2) {
-            return Pair(tail.first + (xDistance / 2), tail.second)
+            return Pair(end.first + (xDistance / 2), end.second)
         }
         if (abs(xDistance) <= 1 && abs(yDistance) <= 1) {
-            return Pair(tail.first, tail.second)
+            return Pair(end.first, end.second)
         }
 
-        return Pair(tail.first + (xDistance / abs(xDistance)),
-            tail.second + (yDistance / abs(yDistance)))
+        return Pair(end.first + (xDistance / abs(xDistance)),
+            end.second + (yDistance / abs(yDistance)))
     }
 }
