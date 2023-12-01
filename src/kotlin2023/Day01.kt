e327f5c816823c1535440833858a72bf043311c0
Yevgen Berberyan <egnbjork@users.noreply.github.com>
2023-12-01 17:31:56 +0100

    2023: day 1 part 2

--- src/kotlin2023/Day01.kt
+++ src/kotlin2023/Day01.kt
@@ -2,17 +2,29 @@ package kotlin2023
 
 import readInput
 
+val spelledNumbers = mapOf(
+        "one" to 1,
+        "two" to 2,
+        "three" to 3,
+        "four" to 4,
+        "five" to 5,
+        "six" to 6,
+        "seven" to 7,
+        "eight" to 8,
+        "nine" to 9
+)
 fun main() {
     val lines = readInput("kotlin2023/Day01_test")
-    val result = lines.map{
+    val result = lines.sumOf {
         it.getDigits()
-    }.sum()
+    }
     println(result)
 }
 
 fun String.getDigits(): Int {
+    val noSpellNumbers = this.parseSpelledNumbers()
     var digits = ""
-    this.forEach {
+    noSpellNumbers.forEach {
         if (it.isDigit()) {
             digits += it
         }
@@ -23,4 +35,16 @@ fun String.getDigits(): Int {
         1 -> (digits + digits).toInt()
         else -> (digits.first() + "" +  digits.last()).toInt()
     }
+}
+
+fun String.parseSpelledNumbers(): String {
+    var result = this
+    spelledNumbers.forEach {
+        if(result.contains(it.key)) {
+            result = result.replace(it.key,
+                    "${it.key.first()}${it.value}${it.key.last()}",
+                    false)
+        }
+    }
+    return result
 }
